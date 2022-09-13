300c0672e9cce24e49a988670b37eccc65ac0c6a
HtoLealMinTic22 <adinhto@gmail.com>
2022-09-12 20:47:55 -0500

    Pruebahto1209222047

--- ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
@@ -5,7 +5,7 @@ namespace ProyectoCiclo3.App.Dominio{
     public class Estaciones{
         public int id {get;set;}
 
-        [Required, StringLength(10,  MinimumLength = 5, ErrorMessage = "Error - Maximo 10 characters")]
+        [Required, StringLength(30,  MinimumLength = 5, ErrorMessage = "Error - Maximo 10 characters")]
         public string nombre {get;set;}            
         public string direccion {get;set;}            
         public string coord_x {get;set;}            
